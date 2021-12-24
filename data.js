8764116e285b668ff14f83d31faf874332a44159
christina-on-github <xtina.ebethsiau@gmail.com>
2021-12-24 17:04:32 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,158 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-atrium",
+      "name": "Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 3.1368521503778943,
+        "pitch": -0.18960068879711578,
+        "fov": 1.6197858818298865
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1993808149665668,
+          "pitch": 0.2954771449663163,
+          "rotation": 0,
+          "target": "1-level-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.1391911561222283,
+          "pitch": -0.14202987539601786,
+          "title": "NUS Business School Atrium",
+          "text": "Welcome to the NUS Business School! This is an open space where students study and have social interaction with their peers.&nbsp;"
+        },
+        {
+          "yaw": 1.4389592461251484,
+          "pitch": -0.5337494087955541,
+          "title": "NUS Business Posters",
+          "text": "Featuring past alumni and students, NUS Business School features outstanding students' achievements on posters and banners placed in the building.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-level-2",
+      "name": "Level 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.3366320708350159,
+        "pitch": -0.03201116109904767,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1339010277361723,
+          "pitch": 0.4334753988531972,
+          "rotation": 0,
+          "target": "2-level-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4982602293187437,
+          "pitch": -0.14241738737413634,
+          "title": "NUS Business School Level 2&nbsp;",
+          "text": "Level 2 features more business school facilities such as Seminar Rooms, Learning Rooms and Study spaces.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "2-level-3",
+      "name": "Level 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.3867007159173532,
+        "pitch": 0.004686636129482835,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8234875126295176,
+          "pitch": 0.14732252040315785,
+          "rotation": 3.9269908169872414,
+          "target": "0-atrium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.616892890327657,
+          "pitch": -0.40830875536093814,
+          "title": "Isetan Foundation Seminar Room&nbsp;",
+          "text": "<div class=\"body\" id=\"yui_3_16_0_1_1640334468450_2357\" style=\"outline-width: 0px;\"><div class=\"content html-only  has-footer\" id=\"yui_3_16_0_1_1640334468450_2356\" style=\"outline-width: 0px; overflow-wrap: break-word; padding: 0px 20px; min-height: 60px; overflow-y: auto; font-size: 14px;\">Isetan Foundation, the philanthropic arm of Isetan (Singapore) Limited established in 1981, made a generous gift to the NUS Business School to set up an endowed scholarship for talented undergraduates. 8 Nov 2018 marks the inaugural naming of a seminar room after a Foundation.</div></div><div class=\"foot\" id=\"yui_3_16_0_1_1640334468450_2359\" style=\"height: 72px; overflow: auto;\"><div class=\"buttons\" id=\"yui_3_16_0_1_1640334468450_2358\" style=\"padding: 20px; display: flex; -webkit-box-pack: justify; justify-content: space-between; -webkit-box-align: center; align-items: center;\"><div class=\"flavor-text-container\" style=\"color: rgb(33, 33, 36); font-family: &quot;Proxima Nova&quot;, &quot;helvetica neue&quot;, helvetica, arial, sans-serif; font-size: medium; background-color: rgb(255, 255, 255);\"></div></div></div>"
+        },
+        {
+          "yaw": -0.3097274137107249,
+          "pitch": -0.21724126291931078,
+          "title": "Seminar Room",
+          "text": "This is one of the seminar rooms that NUS Business students use for their lessons and discussions.&nbsp;"
+        }
+      ]
+    }
+  ],
+  "name": "NUS business tour ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
